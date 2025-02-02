086cade76cdc683fd7c9c7158d73cb36b7389164
staofr <101335940+staofr@users.noreply.github.com>
2025-02-02 17:17:44 +0100

    Stable-4.7.10-Bk

---
+++ themes/simple/components/Footer.js
@@ -0,0 +1,22 @@
+import DarkModeButton from '@/components/DarkModeButton'
+import { siteConfig } from '@/lib/config'
+
+/**
+ * 页脚
+ * @param {*} props
+ * @returns
+ */
+export default function Footer (props) {
+  const d = new Date()
+  const currentYear = d.getFullYear()
+  const since = siteConfig('SINCE')
+  const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
+
+  return <footer className="relative w-full px-6 border-t">
+
+        <div className="text-gray-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('DESCRIPTION')}<a href="https://stao.fr" className=' hover:underline'>.Fr </a> </div>
+                <DarkModeButton className='text-center pt-3'/>
+        </div>
+    </footer>
+}
