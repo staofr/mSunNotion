3d3a7d2acba1e8158a6b2a3cffd30f9e32dd2ab2
stao <101335940+staofr@users.noreply.github.com>
2025-08-21 22:07:16 +0200

    Update Footer.js

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -15,8 +15,9 @@ export default function Footer (props) {
   return <footer className="relative w-full px-6 border-t">
 
         <div className="text-gray-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('DESCRIPTION')}<a href="https://stao.fr" className=' hover:underline'>.Fr </a> </div>
+            <div className='text-center'> &copy;{`${copyrightDate}`} <a href="https://stao.fr" className=' hover:underline'>STAO.FR </a> </div>
                 <DarkModeButton className='text-center pt-3'/>
         </div>
     </footer>
 }
+
