1867e5d6ed7c3f4a2d0fdb988fc9c9881be2a7e2
stao <101335940+staofr@users.noreply.github.com>
2025-08-22 12:09:34 +0200

    Update TopBar.js

--- themes/simple/components/TopBar.js
+++ themes/simple/components/TopBar.js
@@ -9,9 +9,9 @@ export default function TopBar (props) {
   const content = siteConfig('SIMPLE_TOP_BAR_CONTENT', null, CONFIG)
 
   if (content) {
-    return <header className="flex justify-center items-center bg-black dark:bg-hexo-black-gray">
+    return <header className="flex justify-center items-center bg-transparent">
        <div id='top-bar-inner' className='max-w-9/10 w-full z-20'>
-       <div className='text-xs text-center float-left text-white z-50 leading-5 py-2.5' dangerouslySetInnerHTML={{ __html: content }}/>
+       <div className='text-xs text-center float-left text-gray-800 z-50 leading-5 py-2.5' dangerouslySetInnerHTML={{ __html: content }}/>
        </div>
     </header>
   }
